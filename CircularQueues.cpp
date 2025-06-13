ed643c9e20e5cc09182337272e765c858e972538
Miranti20 <rantirarrr4505@gmail.com>
2025-06-13 08:18:12 +0700

    Menambahkan fungsi insert untuk memasukkan elemen ke antrian

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -10,3 +10,29 @@ public:
         FRONT = -1;
         REAR = -1;
     }
+
+    void insert() {
+        int num;
+        cout << "Enter a number: ";
+        cin >>num;
+        cout << endl;
+
+        //Cek apakah antrian penuh
+        if (( FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+            cout << "\nQueues overflow\n";
+            return;
+        }
+
+        // cek apakah antrian kosong 
+        if (FRONT == -1) {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else {
+            // jika REAR berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
